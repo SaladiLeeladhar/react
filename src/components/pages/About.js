75a4f823a8b90ec37c6f51145aa18d5ea258dbe2
saladiLeeladhar <saladileeladhar007@gmail.com>
2020-07-18 15:04:37 +0530

    first commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>this is the todolist app v1.0.0 it is a part of react carsh course</p>
+    </React.Fragment>
+  );
+}
+export default About;
